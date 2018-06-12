7e89f5653a4070ce42277f4336d7ab943f5f4ba9
xukai5265 <xukai5265@126.com>
2018-06-12 14:14:19 +0800

    rdd foreach 写入mysql

---
+++ src/main/scala/cn/xukai/spark/jdbc/Rdd2JDBC.scala
@@ -0,0 +1,23 @@
+package cn.xukai.spark.jdbc
+
+import java.sql
+import java.sql.DriverManager
+
+import org.apache.spark.sql.SparkSession
+
+/**
+  * Created by kaixu on 2018/6/12.
+  */
+object Rdd2JDBC extends App{
+  val spark = SparkSession.builder().master("local[*]").appName("rdd foreach").getOrCreate()
+  val sc = spark.sparkContext
+  val rdd = sc.parallelize(Seq("abc","bcd","def","efg","hig","jkl","mno"),2)
+  rdd.foreach { x =>
+    Class.forName("com.mysql.jdbc.Driver")
+    val conn:sql.Connection = DriverManager.getConnection("jdbc:mysql://localhost/test", "root", "123456")
+    val stmt:sql.Statement = conn.createStatement()
+    stmt.execute(s"insert into rddDemo values('${x}')")
+  }
+
+  spark.close()
+}
